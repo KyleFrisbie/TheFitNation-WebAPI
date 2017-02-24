2804ab0157f4d53092b6462deb68d48846654b8c
michael <mlmmayhem@gmail.com>
2017-02-24 16:37:27 -0700

    added USerCredentialRepo, and USerWeight Repo

---
+++ src/main/java/com/thefitnation/repository/UserCredentialRepo.java
@@ -0,0 +1,52 @@
+package com.thefitnation.repository;
+
+import com.thefitnation.model.*;
+import java.time.*;
+import java.util.*;
+import org.springframework.data.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p></p>
+ * @author michael menard
+ * @version 0.0.1
+ * @since 2/24/17.
+ */
+@Repository
+public interface UserCredentialRepo  extends PagingAndSortingRepository<UserCredential, Long> {
+
+    /**
+     *
+     * @param activationKey
+     * @return
+     */
+    Optional<UserCredential> findOneByActivationKey(String activationKey);
+
+    /**
+     *
+     * @param localDate
+     * @return
+     */
+    List<UserCredential> findAllByActivatedIsFalseAndCreatedDateBefore(LocalDate localDate);
+
+    /**
+     *
+     * @param resetKey
+     * @return
+     */
+    Optional<UserCredential> findOneByResetKey(String resetKey);
+
+    /**
+     *
+     * @param email
+     * @return
+     */
+    Optional<UserCredential> findOneByEmail(String email);
+
+    /**
+     *
+     * @param login
+     * @return
+     */
+    Optional<UserCredential> findOneByLogin(String login);
+}
