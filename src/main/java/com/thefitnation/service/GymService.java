ca023aa1b8fa5d2be3287a0741112cdaea75e18c
michael <mlmmayhem@gmail.com>
2017-02-25 17:50:25 -0700

    moved logic from the controller itno a service class.

--- src/main/java/com/thefitnation/service/GymService.java
+++ src/main/java/com/thefitnation/service/GymService.java
@@ -1,8 +1,12 @@
 package com.thefitnation.service;
 
+import com.thefitnation.model.*;
 import com.thefitnation.repository.*;
+import javax.validation.*;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.*;
 import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by michael on 2/24/17.
@@ -21,4 +25,31 @@ public class GymService {
         this.addressRepo = addressRepo;
     }
 
+
+    public Gym findGyByIdForUser(Long id) {
+        return gymRepo.findOne(id);
+    }
+
+    public Iterable<Gym> getAllGymsForUser() {
+        return gymRepo.findAll();
+    }
+
+    public ResponseEntity<Gym> createGymWithLocaitonAndAddress(@Valid @RequestBody Gym gym) {
+        if (gym.getGymId() != null) {
+            return new ResponseEntity<>(gym, HttpStatus.I_AM_A_TEAPOT);
+        }
+
+        Location location = gym.getGymLocation();
+        Address a = addressRepo.save(location.getAddress());
+
+        location.setAddress(a);
+
+        Location l = locationRepo.save(gym.getGymLocation());
+
+
+        gym.setGymLocation(l);
+
+        Gym result = gymRepo.save(gym);
+        return new ResponseEntity<Gym>(result, HttpStatus.OK);
+    }
 }
