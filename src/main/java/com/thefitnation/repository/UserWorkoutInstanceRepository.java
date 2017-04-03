f776f479cfd23fcef6b2089fc5ad1979d7b85e90
michael <mlmmayhem@gmail.com>
2017-04-02 22:17:15 -0600

    added repo method to get all by current logged in user.

--- src/main/java/com/thefitnation/repository/UserWorkoutInstanceRepository.java
+++ src/main/java/com/thefitnation/repository/UserWorkoutInstanceRepository.java
@@ -1,10 +1,9 @@
 package com.thefitnation.repository;
 
-import com.thefitnation.domain.UserWorkoutInstance;
-
+import com.thefitnation.domain.*;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.*;
-
-import java.util.List;
+import org.springframework.data.repository.query.*;
 
 /**
  * Spring Data JPA repository for the UserWorkoutInstance entity.
@@ -12,4 +11,11 @@ import java.util.List;
 @SuppressWarnings("unused")
 public interface UserWorkoutInstanceRepository extends JpaRepository<UserWorkoutInstance,Long> {
 
+    String FIND_ALL_BY_CURR_USER = "select userWorkoutInstance " +
+        "from UserWorkoutInstance userWorkoutInstance " +
+        "join userWorkoutInstance.userWorkoutTemplate.userDemographic.user u " +
+        "where u.login = :currUser";
+
+    @Query(value = FIND_ALL_BY_CURR_USER)
+    Page<UserWorkoutInstance> findAllByCurrentUser(@Param("currUser") String currUser, Pageable pageable);
 }
