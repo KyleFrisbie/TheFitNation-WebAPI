1920947e659523a41c78897e64bc0ce89c265aea
michael <mlmmayhem@gmail.com>
2017-04-22 15:06:36 -0600

    Exercise instance CRUD

--- src/main/java/com/thefitnation/repository/UserExerciseInstanceRepository.java
+++ src/main/java/com/thefitnation/repository/UserExerciseInstanceRepository.java
@@ -1,10 +1,9 @@
 package com.thefitnation.repository;
 
-import com.thefitnation.domain.UserExerciseInstance;
-
+import com.thefitnation.domain.*;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.*;
-
-import java.util.List;
+import org.springframework.data.repository.query.*;
 
 /**
  * Spring Data JPA repository for the UserExerciseInstance entity.
@@ -12,4 +11,23 @@ import java.util.List;
 @SuppressWarnings("unused")
 public interface UserExerciseInstanceRepository extends JpaRepository<UserExerciseInstance,Long> {
 
+    @Query(
+        "select userExerciseInstance " +
+            "from UserExerciseInstance userExerciseInstance " +
+            "where userExerciseInstance.userWorkoutInstance.userWorkoutTemplate.userDemographic.user.login = :login")
+    Page<UserExerciseInstance> findAll(@Param(value = "login") String login, Pageable pageable);
+
+
+//    @Query("select exerciseInstance " +
+//        "from ExerciseInstance exerciseInstance " +
+//        "where exerciseInstance.workoutInstance.workoutTemplate.userDemographic.user.login = :login " +
+//        "and exerciseInstance.id = :id")
+
+    @Query("select userExerciseInstance " +
+        "from UserExerciseInstance userExerciseInstance " +
+        "where userExerciseInstance.userWorkoutInstance.userWorkoutTemplate.userDemographic.user.login = :login " +
+        "and exerciseInstance.id = :id")
+    UserExerciseInstance findOne(@Param(value = "login") String login, @Param(value = "id") Long id);
+
 }
+
